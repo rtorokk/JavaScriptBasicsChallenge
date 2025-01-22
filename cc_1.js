804328ac2a9554ed7ea56295b8b735ed9fd0e052
Rachael <rachy.torok@gmail.com>
2025-01-22 18:03:34 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,11 @@ console.log("Employee Name:", employeeName, "-", typeof employeeName);
 console.log("Employee ID:", employeeID, "-", typeof employeeID);
 console.log("Active:", isActive, "-", typeof isActive);
 
+// Task 2 - Product Details
+let productName = "Smartphone";
+const productPrice = 500;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "-", typeof productName);
+console.log("Product Price:", productPrice, "-", typeof productPrice);
+console.log("Available:", isAvailable, "-", typeof isAvailable);
