a70ac80059acf66d98bcf605f7a5fb6220c41feb
Rachael <rachy.torok@gmail.com>
2025-01-22 17:56:42 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1 - Employee Information
+let employeeName = "John Smith";
+const employeeID = 101;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "-", typeof employeeName);
+console.log("Employee ID:", employeeID, "-", typeof employeeID);
+console.log("Active:", isActive, "-", typeof isActive);
+
